669c04bdb8054c4d3ffd50279e929946743a1e92
Luis Miguel Valenzuela <lmvalenzuela@ctrl-zeta.com>
2023-02-09 22:39:09 -0600

    - First commit - Started project - Configure eslint, babel, fontawesome and bootstrap - Started main layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid vh-100">
+      <div className="row h-10 border">
+        <div className="col-1">
+          <FontAwesomeIcon icon={solid('star')} />
+        </div>
+        <div className="col-8">Main top area</div>
+        <div className="col-3">
+          <a href="#">Back to Edvisor for Agents</a>
+        </div>
+      </div>
+      <div className="row h-90">
+        <div className="col-2 h-100 border ">Left bar</div>
+        <div className="col h-100 border rightBar">Right bar</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
